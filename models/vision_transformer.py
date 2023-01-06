1c06f4a50157aa43eefb84bf54461832484740cf
ErwanFagnou <r1bengalis@gmail.com>
2023-01-06 21:02:59 +0100

    dropout + ELU (+ fc layers)

--- models/vision_transformer.py
+++ models/vision_transformer.py
@@ -45,32 +45,54 @@ def get_ViT_encoder(dataset: DatasetManager):
 
 def get_CNN_encoder(dataset: DatasetManager):
 
-    class CNNEncoder(nn.Sequential):
+    class CNNEncoder(nn.Module):
+        output_size = 64
         num_channels = [1, 16, 32, 48, 64]
         kernel_sizes = [3, 3, 3, 3]
-        activation_cls = nn.ReLU
+        cnn_activation_cls = nn.ELU
+        fc_activation_cls = nn.ELU
+        dropout_rate = 0.2
 
         config = ViTConfig(
-            hidden_size=num_channels[-1],
+            hidden_size=output_size,
         )
+        main_input_name = 'pixel_values'
 
         def __init__(self):
             super().__init__()
-            self.activation = self.activation_cls()
+            self.cnn_activation = self.cnn_activation_cls()
+            self.fc_activation = self.fc_activation_cls()
+
+            layers = []
             for i in range(len(self.num_channels) - 1):
-                self.add_module(f"conv{i}", nn.Conv2d(self.num_channels[i], self.num_channels[i + 1], self.kernel_sizes[i], padding='same'))
-                self.add_module(f"pool{i}", nn.MaxPool2d(2))
-                self.add_module(f"act{i}", self.activation)
+                layers.append(nn.Conv2d(self.num_channels[i], self.num_channels[i + 1], self.kernel_sizes[i], padding='same'))
+                layers.append(nn.MaxPool2d(2))
+                layers.append(self.cnn_activation)
+            self.cnn = nn.Sequential(*layers)
+
+            self.dropout = nn.Dropout(self.dropout_rate)
+
+            # TODO: layer norm?
+
+            # self.fc1 = nn.Linear(self.num_channels[-1], self.output_size)
+            # self.fc2 = nn.Linear(self.output_size, self.output_size)
 
         def get_output_embeddings(self):
             return None
 
-        def forward(self, x: torch.Tensor, **_):
-            x = super().forward(x)
+        def forward(self, pixel_values: torch.Tensor, **_):
+            x = self.cnn(pixel_values)
+            x = self.dropout(x)
             # shape: (batch_size, channels, height, width)
 
             x = x.flatten(2).transpose(1, 2)
             # shape: (batch_size, height * width, channels)
+
+            # x = self.fc1(x)
+            # x = self.fc_activation(x)
+            # x = self.fc2(x)
+            # x = self.fc_activation(x)
+
             return ModelOutput(last_hidden_state=x, hidden_states=None, attentions=None)
 
     return CNNEncoder()
