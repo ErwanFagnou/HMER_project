47bbc5db97584adfa5eca2d299ff412267f104f3
ErwanFagnou <r1bengalis@gmail.com>
2023-01-06 21:04:10 +0100

    minor fix

--- evaluation.py
+++ evaluation.py
@@ -6,11 +6,13 @@ from models.vision_transformer import TrOCR
 
 
 if __name__ == '__main__':
+
+
     crohme = datasets.DatasetManager(datasets.CROHME, batch_size=config.batch_size, precompute_padding=config.precompute_padding, batch_padding=config.batch_padding, downscale=config.downscale)
     print(crohme.max_img_h, crohme.max_img_w, crohme.max_label_len)
 
     # model = TrOCR(crohme).to(config.device)
-    model = TrOCR.load_from_checkpoint("lightning_logs/version_55/checkpoints/epoch=79-step=11120.ckpt", dataset=crohme).to(config.device)
+    model = TrOCR.load_from_checkpoint("lightning_logs/version_66/checkpoints/epoch=103-step=14456.ckpt", dataset=crohme).to(config.device)
     model.eval()
 
     import matplotlib.pyplot as plt
@@ -20,8 +22,8 @@ if __name__ == '__main__':
     print("Test size 2016:", len(crohme.test_loaders['TEST16'].dataset))
     print("Test size 2019:", len(crohme.test_loaders['TEST19'].dataset))
 
-    loader = crohme.train_loader
-    # loader = crohme.test_loaders["TEST14"]
+    # loader = crohme.train_loader
+    loader = crohme.test_loaders["TEST14"]
 
     with torch.no_grad():
         for imgs, true_outputs in loader:
@@ -29,7 +31,7 @@ if __name__ == '__main__':
                 print(img.shape, true_output.shape)
                 print("True:", [crohme.id2label[i] for i in true_output.cpu().numpy()])
 
-                inputs = img.unsqueeze(0).to(config.device)
+                inputs = img.unsqueeze(0).to(config.device).float()
 
                 # model(inputs, true_output)
                 # print(model.result)
