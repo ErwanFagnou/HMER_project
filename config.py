1ef2199fa2ae7775012d09a081fbf86b032a5bf1
ErwanFagnou <r1bengalis@gmail.com>
2023-01-07 23:22:31 +0100

    Model-V4: ready for training (smaller lr)

--- config.py
+++ config.py
@@ -24,9 +24,9 @@ effective_batch_size = 64
 batch_size = 64
 accumulate_grad_batches = effective_batch_size // batch_size
 optimizer = torch.optim.Adam
-opt_kwargs = dict(lr=1e-3, weight_decay=1e-5)
+opt_kwargs = dict(lr=1e-4, weight_decay=1e-5)
 lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR
-lr_scheduler_kwargs = dict(T_max=epochs, eta_min=1e-5)
+lr_scheduler_kwargs = dict(T_max=epochs, eta_min=1e-6)
 
 # optimizer = torch.optim.SGD
 # opt_kwargs = dict(lr=1e-2, momentum=0.9, weight_decay=0)
