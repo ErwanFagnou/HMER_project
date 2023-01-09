882f6ad5e7e1a546bc3c4a46dfd35be0828af1f0
ErwanFagnou <r1bengalis@gmail.com>
2023-01-09 18:47:04 +0100

    simpler encoder

--- models/vision_transformer.py
+++ models/vision_transformer.py
@@ -53,12 +53,12 @@ class CNNEncoder(nn.Module):
     output_size = 32
     num_channels = [1, 16, 16, 32, 32, output_size]
     kernel_sizes = [4, 4, 4, 4, 4]
-    pooling = [2, 2, 2, 2, 1]
+    pooling = [2, 2, 2, 2, 2]
     # num_channels = [1, 16, 16, 16, 16, 32, 32, 32, output_size]
     # kernel_sizes = [3, 3, 3, 3, 3, 3, 3, 3]
     # pooling = [2, 1, 2, 1, 2, 1, 2, 1]
     dropouts = [0.2, 0.2, 0.2, 0, 0, 0]
-    total_pooling = 16  # prod of pooling
+    total_pooling = 32  # prod of pooling
     cnn_activation_cls = nn.ELU
     fc_activation_cls = nn.ELU
     dropout_rate = 0.2
@@ -79,7 +79,7 @@ class CNNEncoder(nn.Module):
             layers.append(nn.Conv2d(self.num_channels[i], self.num_channels[i + 1], self.kernel_sizes[i], padding='same'))
             layers.append(nn.BatchNorm2d(self.num_channels[i+1]))
             if self.pooling[i] > 1:
-                layers.append(nn.AvgPool2d(self.pooling[i]))
+                layers.append(nn.MaxPool2d(self.pooling[i]))
             layers.append(self.cnn_activation)
             if self.dropouts[i] > 0:
                 layers.append(nn.Dropout(self.dropouts[i]))
