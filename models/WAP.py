5269edaad88eb1c7e526ae6c50e47444780ce99b
ErwanFagnou <r1bengalis@gmail.com>
2023-01-10 12:52:36 +0100

    try clear memory

--- models/WAP.py
+++ models/WAP.py
@@ -1,3 +1,5 @@
+import gc
+
 import torch
 from torch import nn
 from transformers import PretrainedConfig, PreTrainedModel, TrOCRConfig
@@ -72,10 +74,13 @@ class WAPDecoder(PreTrainedModel):
             x = self.dropout(x)
             x = self.attention_proj(x)
             att_weights = torch.softmax(x, dim=1)
+            gc.collect()  # clean memory
             context = torch.sum(att_weights * encoder_outputs, dim=1)
             context = self.context_layernorm(context)
             if self.training:
                 context = context + torch.randn_like(context) * self.noise_std
+
+            gc.collect()  # clean memory
 
             att_weights_cumsum = att_weights_cumsum + att_weights
 
